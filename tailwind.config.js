7f8389e63558de5bd41445d0113d329220f76ea3
rlaxhfn <k048204@hanyang.ac.kr>
2025-07-22 20:55:47 +0900

    docs: [FFBV-51] tailwind 오탈자 및 클래스명 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ export default {
       'title-xl': ['54px', { lineHeight: '65px', letterSpacing: '-0.3px' }],
       'title-lg': ['32px', { lineHeight: '39px', letterSpacing: '-0.2px' }],
       'title-md': ['28px', { lineHeight: '34px', letterSpacing: '-0.2px' }],
-      'titme-sm': ['22px', { lineHeight: '28px', letterSpacing: '-0.2px' }],
+      'title-sm': ['22px', { lineHeight: '28px', letterSpacing: '-0.2px' }],
 
       // body
       body01: ['21px', { lineHeight: '32px', letterSpacing: '0px' }],
@@ -36,21 +36,23 @@ export default {
 
       // etc
       callout: ['16px', { lineHeight: '21px', letterSpacing: '-0.1px' }],
+      footnote: ['14px', { lineHeight: '18px', letterSpacing: '-0.1px' }],
       caption: ['12px', { lineHeight: '16px', letterSpacing: '-0.1px' }],
     },
     extend: {
       colors: {
-        // 주요 컬러 - 일단 그레이로
+        // 주요 컬러 - 일단 스켈레톤 사용 색으로 지정. gray 영역은 tailwind 기본 색상 사용
         primary: {
           ...generateColors('#A2A8AF'),
         },
         secondary: {
           ...generateColors('#E0E2E4'),
         },
-        'text-primary': {
+        // 폰트 컬러
+        'fg-primary': {
           ...generateColors('#25282B'),
         },
-        'text-secondary': {
+        'fg-secondary': {
           ...generateColors('#6D757E'),
         },
         // 기본 상태
