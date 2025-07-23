5c296277d3f0b715aa22565ea39d80d82d0a879f
rlaxhfn <k048204@hanyang.ac.kr>
2025-07-23 22:43:30 +0900

    feat: [FFBV-112] SideBarLayout UI 구현

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -1,15 +1,4 @@
 <!-- 인사이트, 마이페이지 내 사용 -->
-<!-- 사용 방법
-1. 사용하고자 하는 페이지 script에 navItems 정의
- e.g.
- const navItems = [
-  { to: '/', label: '홈' },
-  { to: '/insight', label: '회고 인사이트' },
-  { to: '/products', label: '금융상품 둘러보기' },
-]
-2. 사용하고자 하는 페이지 template에 <SideBar :nav-items="navItems" /> 작성
--->
-
 <script setup>
 import { RouterLink, useRoute } from 'vue-router'
 
@@ -25,9 +14,9 @@ defineProps({
 
 <template>
   <!-- 사이드바 영역 -->
-  <div class="w-1/5 pt-[150px]">
+  <div class="pt-[150px] mobile:hidden">
     <ul
-      class="menu menu-vertical w-full px-8 mobile:hidden text-body02 text-fg-primary"
+      class="menu menu-vertical w-full pl-8 pr-0 py-0 text-body02 text-fg-primary"
       style="
         --menu-active-bg: rgb(162 168 175 / var(--tw-bg-opacity, 1));
         --menu-active-fg: inherit;
---
+++ src/layouts/SideBarLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SideBar from '@/components/layout/SideBar.vue'
+import Header from '@/components/layout/Header.vue'
+import QuickButton from '@/components/layout/QuickButton.vue'
+import Footer from '@/components/layout/Footer.vue'
+import { RouterView, useRoute } from 'vue-router'
+
+const route = useRoute()
+const navItems = route.meta.navItems
+</script>
+
+<template>
+  <Header />
+  <main class="wrapper mt-14 grid grid-cols-5">
+    <SideBar :nav-items="navItems" />
+    <div class="ml-20 mr-[13%] col-span-4">
+      <RouterView />
+    </div>
+  </main>
+  <QuickButton />
+  <Footer />
+</template>
