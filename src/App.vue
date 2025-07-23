4b811ae54e20a6f55347446c279cd981e2bd1caf
rlaxhfn <k048204@hanyang.ac.kr>
2025-07-23 22:43:05 +0900

    feat: [FFBV-112] DefaultLayout UI 구현

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import Header from '@/components/layout/Header.vue'
-import Footer from '@/components/layout/Footer.vue'
-import QuickButton from '@/components/layout/QuickButton.vue'
 </script>
 
 <template>
-  <Header />
-  <div class="wrapper mt-14">
-    <RouterView />
-  </div>
-  <QuickButton />
-  <Footer />
+  <RouterView />
 </template>
 
 <style scoped></style>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from '@/components/layout/Header.vue'
+import QuickButton from '@/components/layout/QuickButton.vue'
+import Footer from '@/components/layout/Footer.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <Header />
+  <main class="wrapper mt-14">
+    <RouterView />
+  </main>
+  <QuickButton />
+  <Footer />
+</template>
