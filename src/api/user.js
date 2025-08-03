63471e0d822fa80915608f6e7523e89f6f624fc7
eunwoo <lkjin0110@gmail.com>
2025-08-03 16:17:55 +0900

    feat: 회원가입 api 호출 코드 구현

--- src/api/user.js
+++ src/api/user.js
@@ -12,3 +12,23 @@ export const getUserProducts = async () => {
     throw error;
   }
 };
+
+export const requestJoin = async (body) => {
+  try {
+    const response = await api.post('/signup/request', body);
+    return response;
+  } catch (error) {
+    console.error('User requestJoin API Error:', error);
+    throw error;
+  }
+};
+
+export const join = async (token, body) => {
+  try {
+    const response = await api.post('/signup?token=' + token, body);
+    return response;
+  } catch (error) {
+    console.error('User Join API Error:', error);
+    throw error;
+  }
+};
