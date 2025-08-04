128d3a61203bb94b9328236fb98dff35d811479f
eunwoo <lkjin0110@gmail.com>
2025-08-04 14:11:28 +0900

    fix: sessionStorage에 username 저장

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -4,7 +4,7 @@ import * as authUtil from '@/utils/authUtil';
 export const useAuthStore = defineStore('auth',  {
   state: () => ({
     isLoggedIn: false,
-    user: null,
+    username: null,
   }),
   actions: {
     initialize() {
@@ -13,30 +13,36 @@ export const useAuthStore = defineStore('auth',  {
         try {
           const parsedToken = authUtil.parseToken(token);
 
-          if (authUtil.isValidToken(parsedToken))
+          if (authUtil.isValidToken(parsedToken)) {
             this.isLoggedIn = true;
+            this.username = authUtil.getUsername();
+          }
           else {
             console.error('만료된 토큰');
             authUtil.clearAccessToken();
             this.isLoggedIn = false;
+            this.username = null;
           }
 
         } catch (e) {
           console.error('토큰 형식 오류: ', e);
           authUtil.clearAccessToken();
           this.isLoggedIn = false;
+          this.username = null;
         }
       }
     },
     login(token, userData) {
       this.isLoggedIn = true;
-      this.user = userData;
+      this.username = userData;
       authUtil.setAccessToken(token);
+      authUtil.setUsername(this.username);
     },
     logout() {
       this.isLoggedIn = false;
-      this.user = null;
+      this.username = null;
       authUtil.clearAccessToken();
+      authUtil.clearUsername();
     }
   }
 })
--- src/utils/authUtil.js
+++ src/utils/authUtil.js
@@ -10,6 +10,18 @@ export const clearAccessToken = () => {
   sessionStorage.removeItem('accessToken');
 };
 
+export const setUsername = (username) => {
+  sessionStorage.setItem('username', username);
+};
+
+export const getUsername = () => {
+  return sessionStorage.getItem('username');
+};
+
+export const clearUsername = () => {
+  sessionStorage.removeItem('username');
+};
+
 export const parseToken = (token) => {
   const payload = token.split('.')[1];
   const decoded = atob(payload.replace(/-/g, '+').replace(/_/g, '/')); // base64 디코딩
