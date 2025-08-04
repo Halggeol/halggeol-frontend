f853905936be1574a72de28db4ffde0e7c3dbf2c
eunwoo <lkjin0110@gmail.com>
2025-08-04 11:48:34 +0900

    fix: 토큰 파싱 함수 분리

--- src/utils/authUtil.js
+++ src/utils/authUtil.js
@@ -9,3 +9,14 @@ export const getAccessToken = () => {
 export const clearAccessToken = () => {
   sessionStorage.removeItem('accessToken');
 };
+
+export const parseToken = (token) => {
+  const payload = token.value.split('.')[1];
+  const decoded = atob(payload.replace(/-/g, '+').replace(/_/g, '/')); // base64 디코딩
+  return JSON.parse(decoded);
+}
+
+export const isValidToken = (parsedToken) => {
+  const now = Math.floor(Date.now() / 1000);
+  return (parsedToken.exp && parsedToken.exp > now);
+}
