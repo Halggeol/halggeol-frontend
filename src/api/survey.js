9ca3b390dbcfdb45daf50d75171fd49ef66a44d7
eunwoo <lkjin0110@gmail.com>
2025-08-07 14:33:10 +0900

    feat: 투자 성향 api 함수 구현

--- src/api/survey.js
+++ src/api/survey.js
@@ -22,3 +22,23 @@ export const updateKnowledgeSurvey = async (body) => {
     throw error;
   }
 };
+
+export const initTendencySurvey = async (body) => {
+  try {
+    const response = await api.patch('/survey/tendency/init', body);
+    return response;
+  } catch (error) {
+    console.error('Init Tendency Survey API Error:', error);
+    throw error;
+  }
+};
+
+export const updateTendencySurvey = async (body) => {
+  try {
+    const response = await api.patch('/survey/tendency', body);
+    return response;
+  } catch (error) {
+    console.error('Tendency Survey API Error:', error);
+    throw error;
+  }
+};
