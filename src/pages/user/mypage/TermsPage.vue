fa23a6347806b7694f82b2e3c077ca4853c3972e
eunwoo <lkjin0110@gmail.com>
2025-08-07 21:28:52 +0900

    feat: 약관 조회 UI 구현

--- src/pages/user/mypage/TermsPage.vue
+++ src/pages/user/mypage/TermsPage.vue
@@ -1,1 +1,40 @@
-<template><h2>terms</h2></template>
+<script setup>
+import { termsText, privacyText } from '@/constants/terms';
+
+const formattedTermsText = termsText.replace(
+  /제\d+조\s*\([^)]+\)/g,
+  (match) => `<div class="title03">${match}</div>`
+);
+
+const formattedPrivacyText = privacyText.replace(
+  /제\d+조\s*\([^)]+\)/g,
+  (match) => `<div class="title03">${match}</div>`
+);
+
+</script>
+
+<template>
+  <div class="w-full mx-auto mt-8 p-6">
+    <!-- 제목 -->
+    <h2 class="title01 font-bold mb-6 text-left">이용약관</h2>
+
+    <!-- 가로 방향 분할 -->
+    <div class="flex flex-col">
+      <!-- 이용약관 -->
+      <div class="flex-1">
+        <div class="mb-10 border rounded p-4 h-[250px] overflow-y-auto text-sm leading-relaxed whitespace-pre-line">
+          <div v-html="formattedTermsText"></div>
+        </div>
+      </div>
+
+    <h2 class="title01 font-bold mb-6 text-left">개인정보 처리방침</h2>
+
+    <!-- 개인정보 처리방침 -->
+      <div class="flex-1">
+        <div class="border rounded p-4 h-[250px] overflow-y-auto text-sm leading-relaxed whitespace-pre-line">
+          <div v-html="formattedPrivacyText"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
