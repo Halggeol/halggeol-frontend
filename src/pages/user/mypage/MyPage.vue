cbff3108b1ed9426da52999b8ec18441ad1e5ecc
eunwoo <lkjin0110@gmail.com>
2025-08-14 15:04:06 +0900

    fix: 마이페이지 클래스명 수정

--- src/pages/user/mypage/MyPage.vue
+++ src/pages/user/mypage/MyPage.vue
@@ -128,34 +128,34 @@ function handleCycleChanged(newCycle) {
 </script>
 
 <template>
-  <div class="w-full mx-auto mt-8 p-6">
+  <div class="h-[calc(100vh-56px-114px)] pt-12 flex flex-col">
     <!-- 제목 -->
-    <h2 class="title01 font-bold mb-2 text-left">내 정보</h2>
+    <h2 class="title02 mb-2 text-left">내 정보</h2>
     <hr class="mb-10"/>
 
     <!-- 정보 리스트 -->
-    <div class="space-y-6 text-sm">
+    <div class="space-y-6 text-body02">
       <!-- 이름 -->
       <div class="flex items-center justify-between w-full">
-        <div class="w-1/3 font-medium text-gray-800">이름</div>
-        <div class="w-1/2 text-gray-900">{{ user.name }}</div>
+        <div class="w-1/3 text-body02 text-fg-secondary">이름</div>
+        <div class="w-1/2 text-fg-primary">{{ user.name }}</div>
         <div class="w-1/3"></div>
       </div>
 
       <!-- 이메일 -->
       <div class="flex items-center justify-between w-full">
-        <div class="w-1/3 font-medium text-gray-800">이메일</div>
-        <div class="w-1/2 text-gray-900">{{ user.email }}</div>
+        <div class="w-1/3 text-body02 text-fg-secondary">이메일</div>
+        <div class="w-1/2 text-fg-primary">{{ user.email }}</div>
         <div class="w-1/3"></div>
       </div>
 
       <!-- 비밀번호 -->
       <div class="flex items-center justify-between w-full">
-        <div class="w-1/3 font-medium text-gray-800">비밀번호</div>
-        <div class="w-1/2 text-gray-900">********</div>
+        <div class="w-1/3 text-body02 text-fg-secondary">비밀번호</div>
+        <div class="w-1/2 text-fg-primary">********</div>
         <div class="w-1/3 text-right">
           <button
-            class="text-gray-400 text-sm hover:text-gray-600 hover:underline"
+            class="text-fg-gray text-body02 hover:text-fg-secondary hover:underline"
             @click="changePassword"
           >
             변경하기
@@ -165,25 +165,25 @@ function handleCycleChanged(newCycle) {
 
       <!-- 전화번호 -->
       <div class="flex items-center justify-between w-full">
-        <div class="w-1/3 font-medium text-gray-800">전화번호</div>
-        <div class="w-1/2 text-gray-900">{{ user.phone }}</div>
+        <div class="w-1/3 text-body02 text-fg-secondary">전화번호</div>
+        <div class="w-1/2 text-fg-primary">{{ user.phone }}</div>
         <div class="w-1/3"></div>
       </div>
 
       <!-- 생년월일 -->
       <div class="flex items-center justify-between w-full">
-        <div class="w-1/3 font-medium text-gray-800">생년월일</div>
-        <div class="w-1/2 text-gray-900">{{ user.birth }}</div>
+        <div class="w-1/3 text-body02 text-fg-secondary">생년월일</div>
+        <div class="w-1/2 text-fg-primary">{{ user.birth }}</div>
         <div class="w-1/3"></div>
       </div>
 
       <!-- 금융이해도 -->
       <div class="flex items-center justify-between w-full">
-        <div class="w-1/3 font-medium text-gray-800">금융이해도</div>
-        <div class="w-1/2 text-gray-900">{{ user.knowledge }}</div>
+        <div class="w-1/3 text-body02 text-fg-secondary">금융이해도</div>
+        <div class="w-1/2 text-fg-primary">{{ user.knowledge }}</div>
         <div class="w-1/3 text-right">
           <button
-            class="text-gray-400 text-sm hover:text-gray-600 hover:underline"
+            class="text-fg-gray text-body02 hover:text-fg-secondary hover:underline"
             @click="retakeKnowledgeTest"
           >
             재검사하기
@@ -193,11 +193,11 @@ function handleCycleChanged(newCycle) {
 
       <!-- 투자성향 -->
       <div class="flex items-center justify-between w-full">
-        <div class="w-1/3 font-medium text-gray-800">투자성향</div>
-        <div class="w-1/2 text-gray-900">{{ user.investmentType }}</div>
+        <div class="w-1/3 text-body02 text-fg-secondary">투자성향</div>
+        <div class="w-1/2 text-fg-primary">{{ user.investmentType }}</div>
         <div class="w-1/3 text-right">
           <button
-            class="text-gray-400 text-sm hover:text-gray-600 hover:underline"
+            class="text-fg-gray text-body02 hover:text-fg-secondary hover:underline"
             @click="retakeTendencyTest"
           >
             재검사하기
@@ -207,11 +207,11 @@ function handleCycleChanged(newCycle) {
 
       <!-- 인사이트 주기 -->
       <div class="flex items-center justify-between w-full">
-        <div class="w-1/3 font-medium text-gray-800">인사이트 제공 주기</div>
-        <div class="w-1/2 text-gray-900">{{ convertCycleString(user.insightCycle) }}</div>
+        <div class="w-1/3 text-body02 text-fg-secondary">인사이트 제공 주기</div>
+        <div class="w-1/2 text-fg-primary">{{ convertCycleString(user.insightCycle) }}</div>
         <div class="w-1/3 text-right">
           <button
-            class="text-gray-400 text-sm hover:text-gray-600 hover:underline"
+            class="text-fg-gray text-body02 hover:text-fg-secondary hover:underline"
             @click="changeInsightCycle"
           >
             변경하기
@@ -222,7 +222,7 @@ function handleCycleChanged(newCycle) {
 
     <!-- 탈퇴하기 버튼 -->
     <div class="my-28">
-      <button class="text-sm text-gray-400 hover:underline" @click="leaveService">
+      <button class="text-body02 text-fg-gray hover:underline" @click="leaveService">
         탈퇴하기
       </button>
     </div>
