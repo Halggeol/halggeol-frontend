a7d5a9b62d5ed704ac158ae115c9d5c35ceaf576
eunwoo <lkjin0110@gmail.com>
2025-08-14 14:49:53 +0900

    fix: 약관 조회 UI 수정

--- src/pages/user/mypage/TermsPage.vue
+++ src/pages/user/mypage/TermsPage.vue
@@ -13,18 +13,18 @@ function formatSectionTitles(text) {
 <template>
   <div class="h-[calc(100vh-56px-114px)] pt-12 flex flex-col">
     <!-- 제목 -->
-    <h2 class="title02 font-bold mb-6 text-left">이용약관</h2>
+    <h2 class="title02 text-fg-primary mb-6 text-left">이용약관</h2>
 
       <!-- 이용약관 -->
-      <div class="text-callout mb-10 border rounded p-4 h-[250px] overflow-y-auto leading-relaxed whitespace-pre-line">
+      <div class="text-callout text-fg-primary mb-10 border rounded p-4 h-[250px] overflow-y-auto leading-relaxed whitespace-pre-line">
         <div v-html="formatSectionTitles(termsText)"></div>
       </div>
 
     <!-- 제목 -->
-    <h2 class="title02 font-bold mb-6 text-left">개인정보 처리방침</h2>
+    <h2 class="title02 text-fg-primary mb-6 text-left">개인정보 처리방침</h2>
 
       <!-- 개인정보 처리방침 -->
-      <div class="text-callout border rounded p-4 h-[250px] overflow-y-auto leading-relaxed whitespace-pre-line">
+      <div class="text-callout text-fg-primary mb-10 border rounded p-4 h-[250px] overflow-y-auto leading-relaxed whitespace-pre-line">
         <div v-html="formatSectionTitles(privacyText)"></div>
       </div>
     </div>
