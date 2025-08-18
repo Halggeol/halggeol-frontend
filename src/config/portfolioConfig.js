8ee6b88adee606b13c4914d936712504f357c169
rlaxhfn <k048204@hanyang.ac.kr>
2025-08-18 16:53:18 +0900

    refactor: 오탈자 수정

--- src/config/portfolioConfig.js
+++ src/config/portfolioConfig.js
@@ -1,12 +1,12 @@
 export const portfolioConfig = {
-  cash: { name: '계좌·현금', color: 'bg-cash' },
+  // cash: { name: '계좌·현금', color: 'bg-cash' },
   savings: { name: '적금', color: 'bg-savings' },
-  deposit: { name: '예금', color: 'bg-deopsit' },
+  deposit: { name: '예금', color: 'bg-deposit' },
   fund: { name: '펀드', color: 'bg-fund' },
   forex: { name: '외환', color: 'bg-forex' },
   pension: { name: '연금', color: 'bg-pension' },
   aggressive: { name: '공격형 연금', color: 'bg-pension' },
 
   // 기본값 설정
-  default: { name: '기타', color: 'bg-fg-gray' },
+  default: { name: '계좌·현금', color: 'bg-cash' },
 };
